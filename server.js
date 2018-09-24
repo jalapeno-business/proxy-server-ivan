bed7c59f3881aad3e9d4a9ece0e995dc2335b920
Ivan Hui <vanhui@ucdavis.edu>
2018-09-23 19:10:38 -0700

    server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/:id',express.static(path.join(__dirname, 'public')));
 app.all('/api/restaurant/reviews/*', (req, res) => res.redirect(`http://reviews-api.us-west-2.elasticbeanstalk.com/${req.path}`));
 app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://zagat-info2.us-west-1.elasticbeanstalk.com/${req.path}`));
 app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://zagat-suggestions-dev.us-west-1.elasticbeanstalk.com/${req.path}`));
-app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev6.us-west-1.elasticbeanstalk.com/${req.path}`));
+app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev7.us-west-1.elasticbeanstalk.com/${req.path}`));
 app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://rec-public-dev.us-west-1.elasticbeanstalk.com/${req.path}`));
 
 
